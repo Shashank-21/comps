01d8eeddba557b6fa11d7765827cca21710f6da9
Shashank B R <shashankbr21494@gmail.com>
2022-12-08 20:15:11 +0530

    Using immer

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,3 +1,4 @@
+import produce from "immer";
 import { useReducer } from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
@@ -12,20 +13,25 @@ const COUNTER_ACTION_TYPES = {
 const reducer = (state, action) => {
   switch (action.type) {
     case COUNTER_ACTION_TYPES.INCREMENT_COUNT:
-      return { ...state, count: state.count + 1 };
+      state.count = state.count + 1;
+      return;
     case COUNTER_ACTION_TYPES.DECREMENT_COUNT:
-      return { ...state, count: state.count - 1 };
+      state.count = state.count - 1;
+      return;
     case COUNTER_ACTION_TYPES.SET_VALUE_TO_ADD:
-      return { ...state, valueToAdd: action.payload };
+      state.valueToAdd = action.payload;
+      return;
     case COUNTER_ACTION_TYPES.ADD_VALUE_TO_COUNT:
-      return { ...state, count: state.count + state.valueToAdd, valueToAdd: 0 };
+      state.count = state.count + state.valueToAdd;
+      state.valueToAdd = 0;
+      return;
     default:
-      return state;
+      return;
   }
 };
 
 function CounterPage({ initialCount }) {
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, dispatch] = useReducer(produce(reducer), {
     count: initialCount,
     valueToAdd: 0,
   });
